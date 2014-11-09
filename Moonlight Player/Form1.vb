3d4f411bdbb9e67ab2a1d3ed90d4cd0222ddaaed
LY <lywjk@outlook.com>
2014-11-09 11:24:34 +0800

    commit

--- Moonlight Player/Form1.vb
+++ Moonlight Player/Form1.vb
@@ -1,7 +1,8 @@
-﻿Imports Moonlight_Player.libZPlay
-
+﻿Imports Un4seen.Bass
+Imports Un4seen.Bass.BassNet
+'BassNet.Registration("lywjk@outlook.com", "2X534201724822")
 Public Class Form1
-    Dim player As New ZPlay
+
     Dim i As Integer = 0
 
 #Region "界面部分"
@@ -63,82 +64,151 @@ Public Class Form1
 #End Region
 
     Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BassNet.Registration("lywjk@outlook.com", "2X534201724822")
+        Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, Me.Handle)
         Form2.Show()
         Form2.Top = Me.Top + Me.Height + 1
         Form2.Left = Me.Left
+        Dim Form3 As New Form
+        Dim listbox2 = New DvsAlphaListBox
+        listbox2.Location = New Point(0, 0)
+        Form3.Controls.Add(listbox2)
+        Form3.Show()
     End Sub
 
     Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        Dim musicstream As Integer
+
         If btnPlay.Text = "播放" Then
-            If ProgressBar1.Value = 0 Then
+                If ProgressBar1.Value = 0 Then
                 If Form2.playlist.Items.Count <> 0 And Form2.playlist.SelectedItem <> "" Then
-                    player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
-                    player.StartPlayback()
+                    musicstream = Bass.BASS_StreamCreateFile(Form2.playlist.SelectedItem.ToString, 0, 0, BASSFlag.BASS_DEFAULT)
+                    If musicstream <> 0 Then
+                        ' play the stream channel
+                        Bass.BASS_ChannelPlay(musicstream, True)
+                    Else
+                        ' error creating the stream
+                        Console.WriteLine("Stream error: {0}", Bass.BASS_ErrorGetCode())
+                    End If
                     Timer1.Enabled = True
                     btnPlay.Text = "暂停"
                 End If
-            Else
-                player.ResumePlayback()
-            End If
+                Else
+                    Bass.BASS_ChannelPlay(Form2.playlist.SelectedItem, False)
+                End If
         Else
-            player.PausePlayback()
+            'Bass.BASS_ChannelPause()
             btnPlay.Text = "播放"
         End If
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Dim pos As New TStreamTime()
-        Dim StreamInfo As New TStreamInfo()
 
-        player.GetStreamInfo(StreamInfo)
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = CInt(Fix(StreamInfo.Length.sec))
-        player.GetPosition(pos)
+        'ProgressBar1.Minimum = 0
+        'ProgressBar1.Maximum = CInt(Fix(StreamInfo.Length.sec))
 
-        If ProgressBar1.Maximum > CInt(Fix(pos.sec)) Then
-            ProgressBar1.Value = CInt(Fix(pos.sec))
-        Else
-            i += 1
-            player.OpenFile(Form2.playlist.Items.Item(i), TStreamFormat.sfAutodetect)
-        End If
+        'If ProgressBar1.Maximum > CInt(Fix(pos.sec)) Then
+        'ProgressBar1.Value = CInt(Fix(pos.sec))
+        'Else
+        'i += 1
+        'Bass.BASS_ChannelPlay(Form2.playlist.Items.Item(i), True)
+        'End If
     End Sub
 
     Private Sub btncls_Click(sender As Object, e As EventArgs) Handles btncls.Click
         Me.Close()
     End Sub
 
-    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
-        player.StopPlayback()
-    End Sub
+    'Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+    '    player.StopPlayback()
+    'End Sub
 
-    Private Sub btnPre_Click(sender As Object, e As EventArgs) Handles btnPre.Click '上一首按钮
-        Dim pre As Integer
-        pre = Form2.playlist.SelectedIndex - 1
-        If pre >= 0 Then
-            Form2.playlist.SetSelected(pre, True)
-            player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
-            player.StartPlayback()
-        Else
-            pre = 0
-            Form2.playlist.SetSelected(pre, True)
-            player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
-            player.StartPlayback()
-        End If
+    'Private Sub btnPre_Click(sender As Object, e As EventArgs) Handles btnPre.Click '上一首按钮
+    '    Dim pre As Integer
+    '    pre = Form2.playlist.SelectedIndex - 1
+    '    If pre >= 0 Then
+    '        Form2.playlist.SetSelected(pre, True)
+    '        player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
+    '        player.StartPlayback()
+    '    Else
+    '        pre = 0
+    '        Form2.playlist.SetSelected(pre, True)
+    '        player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
+    '        player.StartPlayback()
+    '    End If
 
-    End Sub
+    'End Sub
 
-    Private Sub btnNxt_Click(sender As Object, e As EventArgs) Handles btnNxt.Click '下一首按钮
-        Dim nxt As Integer
-        nxt = Form2.playlist.SelectedIndex + 1
-        If nxt <= Form2.playlist.Items.Count - 1 Then
-            Form2.playlist.SetSelected(nxt, True)
-            player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
-            player.StartPlayback()
-        Else
-            nxt = 0
-            Form2.playlist.SetSelected(nxt, True)
-            player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
-            player.StartPlayback()
-        End If
-    End Sub
+    'Private Sub btnNxt_Click(sender As Object, e As EventArgs) Handles btnNxt.Click '下一首按钮
+    '    Dim nxt As Integer
+    '    nxt = Form2.playlist.SelectedIndex + 1
+    '    If nxt <= Form2.playlist.Items.Count - 1 Then
+    '        Form2.playlist.SetSelected(nxt, True)
+    '        player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
+    '        player.StartPlayback()
+    '    Else
+    '        nxt = 0
+    '        Form2.playlist.SetSelected(nxt, True)
+    '        player.OpenFile(Form2.playlist.SelectedItem, TStreamFormat.sfAutodetect)
+    '        player.StartPlayback()
+    '    End If
+    'End Sub
+
+    Public Class DvsAlphaListBox
+        Inherits ListBox
+        Private doc As Object
+        Public Sub New()
+            AddAlpha()
+            Me.DrawMode = Windows.Forms.DrawMode.OwnerDrawFixed
+            Me.DoubleBuffered = True
+            Me.ItemHeight = 30
+            Me.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, _
+            System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        End Sub
+        Private Sub AddAlpha()
+            Me.SetStyle(ControlStyles.UserPaint, True)
+            Me.SetStyle(ControlStyles.SupportsTransparentBackColor, True)
+            Me.SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+            Me.SetStyle(ControlStyles.DoubleBuffer, True)
+        End Sub
+        Protected Overrides Sub OnSelectedIndexChanged(e As EventArgs)
+            Me.Invalidate()
+            MyBase.OnSelectedIndexChanged(e)
+        End Sub
+        Protected Overrides Sub OnPaint(e As PaintEventArgs)
+            AddAlpha()
+            If Me.Focused AndAlso Me.SelectedItem IsNot Nothing Then
+                Dim itemRect As Rectangle = Me.GetItemRectangle(Me.SelectedIndex)
+                e.Graphics.FillRectangle(Brushes.LightBlue, itemRect)
+            End If
+            Dim i As Integer = 0
+            While i < Items.Count
+                Dim strFmt As StringFormat = New System.Drawing.StringFormat()
+                strFmt.LineAlignment = StringAlignment.Center
+                e.Graphics.DrawString(Me.GetItemText(Me.Items(i)), Me.Font, New SolidBrush(Me.ForeColor), Me.GetItemRectangle(i), strFmt)
+                System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1)
+            End While
+            MyBase.OnPaint(e)
+        End Sub
+        Private Sub Me_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+            '这里是单击右键能够在列表中选中的列表项上面弹出菜单 的 源代码
+            On Error Resume Next
+            If e.Button = Windows.Forms.MouseButtons.Right Then
+                Dim ht As Integer = Me.ItemHeight
+                Dim rect As New Rectangle(0, 0, Me.ClientSize.Width, ht)
+                For SelectListItem As Integer = 0 To Me.Items.Count - 1
+                    If rect.Contains(e.Location) Then
+                        Me.SelectedIndex = SelectListItem + Me.TopIndex
+                        Me.SetSelected(Me.SelectedIndex, True)
+                        Exit For
+                    Else
+                        rect.Y += ht
+                    End If
+                Next SelectListItem
+                ht = Nothing
+                rect = Nothing
+            End If
+        End Sub
+    End Class
+
 End Class
--- Moonlight Player/Form2.vb
+++ Moonlight Player/Form2.vb
@@ -1,4 +1,5 @@
-﻿Public Class Form2
+﻿Imports System.IO
+Public Class Form2
 
     Private Sub btnAddFile_Click(sender As Object, e As EventArgs) Handles btnAddFile.Click
         Dim OpenDlg = New OpenFileDialog
@@ -16,7 +17,28 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim OpenFldDlg As New FolderBrowserDialog
         If OpenFldDlg.ShowDialog() = Windows.Forms.DialogResult.OK Then
-            'For Each filename In OpenFldDlg.SelectedPath
+            OpenFldDlg.Description = "选择音乐文件夹导入音乐文件"
+            GetAllFile(OpenFldDlg.SelectedPath)
+        End If
+    End Sub
+
+    Private Sub GetAllFile(ByVal path As String)
+        Dim strDir As String() = Directory.GetDirectories(path)
+        Dim strFile As String() = Directory.GetFiles(path)
+        Dim extname As String
+        Dim i As Integer
+        If strFile.Length > 0 Then
+            For i = 0 To strFile.Length - 1
+                extname = strFile(i).Substring(InStrRev(strFile(i), "."), strFile(i).Length - InStrRev(strFile(i), "."))
+                If extname = "mp3" Or extname = "flac" Or extname = "ac3" Or extname = "ogg" Then
+                    playlist.Items.Add(strFile(i))
+                End If
+            Next
+        End If
+        If strDir.Length > 0 Then
+            For i = 0 To strDir.Length - 1
+                GetAllFile(strDir(i))
+            Next
         End If
     End Sub
 End Class
